0da1ae8d5bbaa7881853baa55e43f63f055ac63e
siniv <srinivasansuresh5595@gmail.com>
2025-03-10 18:35:30 +0530

    Updated Code for Text to Morse Code

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-print("Text to Morse Code process has Started")
+morse_dict = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',  
+    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',  
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',  
+    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',  
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',  
+    'Z': '--..',  
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',  
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',  
+    ' ': '/'  # Space between words
+}
 
-print("project updated")
+input_text = input("Enter the text in English to convert to MORSE code - ")
+
+print(f"The text entered - {input_text}" )
+
+temp_text = input_text
+
+input_text = input_text.upper()
+
+morse_code = ""
+
+for letter in input_text:
+
+    morse_letter = morse_dict[letter]
+    morse_code = morse_code+morse_letter
+
+print(f"The Norse Code for the text {temp_text} is {morse_code}")
